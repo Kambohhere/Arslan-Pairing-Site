3b6a720937b7d68c278ed4203d9daa37c7dc50c3
Kambohhere <opnot8180@gmail.com>
2026-02-19 08:13:38 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function Arslan_MD_QR_CODE() {
+	async function UMAIR_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Arslan_Tech = Arslan_Tech({
+			let Qr_Code_By_UMAIR_Tech = UMAID_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Arslan_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Arslan_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_UMAID_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_UMAIR_Tech.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,13 +56,13 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Arslan_Tech.sendMessage(Qr_Code_By_Arslan_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
+				   let session = await Qr_Code_By_UMAIR_Tech.sendMessage(Qr_Code_By_Arslan_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
 	
 				   let Arslan_MD_TEXT = `
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨Arslan-MD🔷
-║ ✨ArslanMD OFFICIAL🔷
+║ ✨UMAIR-MD🔷
+║ ✨UMAIRMD OFFICIAL🔷
 ╚════════════════════╝
 
 
@@ -75,31 +75,31 @@ router.get('/', async (req, res) => {
 ╚════════════════════╝
 ╔════════════════════◇
 ║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@arslanmdofficial
-║❍ 𝐎𝐰𝐧𝐞𝐫: 923237045919
+║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@umairahmedyt
+║❍ 𝐎𝐰𝐧𝐞𝐫: 923453897509
 ║❍ 𝐑𝐞𝐩𝐨: https://github.com/Arslan-MD/Arslan_MD 
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/KRyARlvcUjoIv1CPSSyQA5?mode=wwt
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306
-║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/arslanmd
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/Kgg8V9aATB5IC6eEj04CRX?mode=gi_t
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029Vb7Q0pB6LwHmmwCoAw2L
+║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/umairmd
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
-𒂀 Enjoy Arslan-MD
+𒂀 Enjoy UMAIR-MD
 
 
 ---
 
 Don't Forget To Give Star⭐ To My Repo
 ______________________________`;
-	 await Qr_Code_By_Arslan_Tech.sendMessage(Qr_Code_By_Arslan_Tech.user.id,{text:Arslan_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_UMAIR_Tech.sendMessage(Qr_Code_By_UMAIR_Tech.user.id,{text:Arslan_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Arslan_Tech.ws.close();
+					await Qr_Code_By_UMAIR_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					Arslan_MD_QR_CODE();
+					UMAIR_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -112,6 +112,6 @@ ______________________________`;
 			await removeFile("temp/" + id);
 		}
 	}
-	return await Arslan_MD_QR_CODE()
+	return await UMAIR_MD_QR_CODE()
 });
 module.exports = router
